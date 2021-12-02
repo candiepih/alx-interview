5586030ca3e5398506553e0c059c74af3785223e
Candiepih <mutheeal.am@gmail.com>
2021-12-02 19:30:47 +0300

    Formatted file to pep8 linting

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -12,14 +12,16 @@ def canUnlockAll(boxes):
     Returns:
         True if you can open all the lockboxes, False otherwise
     """
-    unlocked_boxes = [0]
-    for box_id, box in enumerate(boxes):
-        if not box:
-            continue
-        for key in box:
-            if key < len(boxes) and key not in unlocked_boxes and key != box_id:
-                unlocked_boxes.append(key)
-            if len(unlocked_boxes) == len(boxes):
-                return True
+    position = 0
+    unlocked = {}
 
+    for box in boxes:
+        if len(box) == 0 or position == 0:
+            unlocked[position] = "always_unlocked"
+        for key in box:
+            if key < len(boxes) and key != position:
+                unlocked[key] = key
+        if len(unlocked) == len(boxes):
+            return True
+        position += 1
     return False
