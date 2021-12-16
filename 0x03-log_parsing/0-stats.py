c33bc4086ea9ab26ce27f19f107ecbabe76e6168
Candiepih <mutheeal.am@gmail.com>
2021-12-16 19:00:07 +0300

    Formatted file to pep8 linting

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -28,12 +28,12 @@ if __name__ == "__main__":
             log_list = re.split('- | "|" | " " ', str(std_line))
             try:
                 codes = log_list[-1].split(" ")
-                if print_counter % 10 == 0 and print_counter != 0:
-                    print_logs()
-                if int(codes[0]) in status_codes:
+                if int(codes[0]) in status_codes.keys():
                     status_codes[int(codes[0])] += 1
                 print_counter += 1
                 size_summation += int(codes[1])
+                if print_counter % 10 == 0 and print_counter != 0:
+                    print_logs()
             except():
                 pass
         print_logs()
