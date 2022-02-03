1e5fa2cbdd11dcdc573fc769ab99805cc63caf6a
Candiepih <mutheeal.am@gmail.com>
2022-02-04 00:49:53 +0300

    Added guard for one island

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -5,6 +5,27 @@ island described in grid
 """
 
 
+def check_perimeter(grid, i, j):
+    """
+    check surroundings for water. If water body
+    on boundary, add 1 to perimeter otherwise add 0
+    """
+    total = 0
+    if i == 0 or i == len(grid) - 1:
+        total += 1
+    else:
+        top = grid[i - 1][j] ^ 1
+        bottom = grid[i + 1][j] ^ 1
+        total += top + bottom
+    if j == 0 or j == len(grid[i]) - 1:
+        total += 1
+    else:
+        left = grid[i][j - 1] ^ 1
+        right = grid[i][j + 1] ^ 1
+        total += left + right
+    return total
+
+
 def island_perimeter(grid):
     """
     Calculates the perimeter of an island given a grid
@@ -19,10 +40,6 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                top = grid[i - 1][j] ^ 1
-                bottom = grid[i + 1][j] ^ 1
-                left = grid[i][j - 1] ^ 1
-                right = grid[i][j + 1] ^ 1
-                perimeter += top + bottom + left + right
+                perimeter += check_perimeter(grid, i, j)
 
     return perimeter
