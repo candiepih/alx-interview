288386d483c5791c43994ffe7164eed481595d85
Candiepih <mutheeal.am@gmail.com>
2022-02-10 14:08:44 +0300

    Added check for a tie

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -19,7 +19,7 @@ def prime_numbers_between(n):
 
     for i in range(2, n + 1):
         is_prime = True
-        for j in range(2, (i // 2) + 1):
+        for j in range(2, i // 2 + 1):
             if i % j == 0:
                 is_prime = False
                 break
